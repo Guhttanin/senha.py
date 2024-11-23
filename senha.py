2cdc1473b55248cb6c757312e20074be3fb3fa15
Guhttanin <gh.203.fe.eb@gmail.com>
2024-11-23 20:07:51 -0300

    Atualizar o senha.py

--- senha.py
+++ senha.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 
-import os
-import time
+def main():
+    import os
+    import time
 
-os.system("clear")
+    os.system("clear")
 
-senha = input("Qual é a senha? Digite 'dica' para obter uma dica: ").upper()
+    senha = input("Qual é a senha? Digite 'dica' para obter uma dica: ").upper()
     
-if senha == "RELÓGIO":
-    print("Você descobriu a senha!")
-    exit()
-    
-if senha == "DICA":
-    senha_dica = input("Dica: conta as horas. Qual é a senha? ").upper()
-    if senha_dica == "RELÓGIO":
+    if senha == "RELÓGIO":
         print("Você descobriu a senha!")
         exit()
-    if senha_dica != "RELÓGIO":
-        print("Essa não é a senha!")
-        exit()        
     
-if senha != "RELÓGIO" and "DICA":
-    print("A senha não é essa!")
-    exit()
+    if senha == "DICA":
+        senha_dica = input("Dica: conta as horas. Qual é a senha? ").upper()
+        if senha_dica == "RELÓGIO":
+            print("Você descobriu a senha!")
+            exit()
+        if senha_dica != "RELÓGIO":
+            print("Essa não é a senha!")
+            exit()        
+    
+    if senha != "RELÓGIO" and "DICA":
+        print("A senha não é essa!")
+        exit()
+
+if __name__ == "__main__":
+    main()
+
 
 
     
